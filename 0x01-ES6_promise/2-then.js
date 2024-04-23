ffac7794291f7333f152ffddebce95d2cbf7683f
MayDan12 <oluwamayowadan@gmail.com>
2024-04-23 21:55:11 +0000

    thens

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,3 +1,3 @@
 export default function handleResponseFromApi(promise) {
-  return promise.then({ status: 200, body: 'success' }).catch(new Error({})).finally(console.log('Got a response from the API'));
+  return promise.then({ status: 200, body: 'success' }).catch(new Error('')).finally(console.log('Got a response from the API'));
 }

f2f4ffae98511b7819993d22f3eb0da661e46d45
MayDan12 <oluwamayowadan@gmail.com>
2024-04-23 10:48:29 +0000

    Working with 9

--- 0x00-ES6_basic/0-main.js
+++ 0x00-ES6_basic/0-main.js
@@ -1,11 +1,6 @@
-import createEmployeesObject from './11-createEmployeesObject.js';
-import createReportObject from './12-createReportObject.js';
+import getFullBudgetObject from './9-getFullBudget.js';
 
-const employees = {
-    ...createEmployeesObject('engineering', ['Bob', 'Jane']),
-    ...createEmployeesObject('marketing', ['Sylvie'])
-};      
+const fullBudget = getFullBudgetObject(20, 50, 10);
 
-const report = createReportObject(employees);
-console.log(report.allEmployees);
-console.log(report.getNumberOfDepartments(report.allEmployees));
+console.log(fullBudget.getIncomeInDollars(fullBudget.income));
+console.log(fullBudget.getIncomeInEuros(fullBudget.income));

04ff736c33d451f863a371f0183780ee3efa7b53
MayDan12 <oluwamayowadan@gmail.com>
2024-04-23 21:51:24 +0000

    hum

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,8 +1,5 @@
-import { uploadPhoto, createUser } from './utils.js'
-
+import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  Promise.all([uploadPhoto(), createUser()])
-    .then((data) => {console.log(`${data[0].body} ${data[1].firstName} ${data[1].lastName}`);})
-    .catch(new Error('Signup system offline'));
-};
+  return Promise.all([uploadPhoto(), createUser()]).then((data) => { console.log(`${data[0].body} ${data[1].firstName} ${data[1].lastName}`); }).catch(new Error('Signup system offline'));
+}

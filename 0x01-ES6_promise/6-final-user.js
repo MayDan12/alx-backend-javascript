c380e047b25014dbdd584cffdf275bbdf0601757
MayDan12 <oluwamayowadan@gmail.com>
2024-04-24 21:56:18 +0000

    6-final-user.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,7 +1,7 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, filename) {
+export default async function handleProfileSignup(firstName, lastName, filename) {
   const promise = [signUpUser(firstName, lastName), uploadPhoto(filename)];
   return Promise.allSettled(promise)
     .then((data) => {

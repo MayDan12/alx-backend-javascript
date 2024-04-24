dac911c2e994be9239f6bd6c4676af0f2a7d1179
MayDan12 <oluwamayowadan@gmail.com>
2024-04-24 22:18:16 +0000

    9-try.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,17 +3,12 @@ import uploadPhoto from './5-photo-reject';
 
 export default async function handleProfileSignup(firstName, lastName, filename) {
   const promise = [signUpUser(firstName, lastName), uploadPhoto(filename)];
-  return Promise.allSettled(promise)
-    .then((data) => {
-    //   console.log(data[0].value, data[0].status, data[1].status, data[1].reason);
-      const newInfo = [];
-      for (const res of data) {
-        if (res.status === 'fulfilled') {
-          newInfo.push({ status: res.status, value: res.value });
-        } else if (res.status === 'rejected') {
-          newInfo.push({ status: res.status, value: res.reason });
-        }
-      }
-      return newInfo;
-    });
+  return Promise
+    .allSettled(promise)
+    .then((data) => (
+      data.map((o) => ({
+        status: o.status,
+        value: o.status === 'fulfilled' ? o.value : String(o.reason),
+      }))
+    ));
 }

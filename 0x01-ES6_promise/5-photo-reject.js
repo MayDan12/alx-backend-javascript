990db2da650700df0b19194c5c548d52b3a823df
MayDan12 <oluwamayowadan@gmail.com>
2024-04-24 11:32:51 +0000

    good

--- 0x01-ES6_promise/5-photo-reject.js
+++ 0x01-ES6_promise/5-photo-reject.js
@@ -1,5 +1,3 @@
-export default function uploadPhoto(filename) {
-  return new Promise((reject) => {
-    reject(new Error(`${filename} cannot be processed`));
-  });
+export default function uploadPhoto(fileName) {
+  return Promise.reject(new Error(`${fileName} cannot be processed`));
 }

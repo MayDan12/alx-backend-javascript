8ca2d66ab18d5ef11e4b448853d129b8f6871b5a
MayDan12 <oluwamayowadan@gmail.com>
2024-04-24 12:06:44 +0000

    6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,19 +3,26 @@ import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, filename) {
   const promise = [signUpUser(firstName, lastName), uploadPhoto(filename)];
-  return Promise.allSettled(promise).then(results => {
-    return results.map(result => {
-      if (result.status === "fulfilled") {
-        return {
-          status: "fulfilled",
-          value: result.value
-        };
-      } else {
-        return {
-          status: "rejected",
-          value: result.reason
-        };
-      }
-    });
+  return Promise.allSettled(promise)
+  .then((results) => {
+      const response = [];
+      results.forEach((result) => {
+          if (result.status === "fulfilled") {
+              response.push({
+                  status: "fulfilled",
+                  value: result.value,
+              });
+          } else {
+              response.push({
+                  status: "rejected",
+                  value: result.reason,
+              });
+          }
+      });
+      return response;
+  })
+  .catch((error) => {
+      console.error("Signup system offline");
+      return [];
   });
 }

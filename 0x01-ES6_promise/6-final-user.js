22b603118b9a6c48ea8cd6d2e6f4f1593bc06a0b
MayDan12 <oluwamayowadan@gmail.com>
2024-04-24 21:11:12 +0000

    6-final-user.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -5,15 +5,16 @@ export default function handleProfileSignup(firstName, lastName, filename) {
   const promise = [signUpUser(firstName, lastName), uploadPhoto(filename)];
   return Promise.allSettled(promise)
     .then((data) => {
-      if (data) {
+    //   console.log(data[0].value, data[0].status, data[1].status, data[1].reason);
+      if (data.status) {
         return [{
-          status: data.status,
-          value: data.value,
+          status: data[0].status,
+          value: data[0].value,
         }];
       }
       return [{
-        status: data.status,
-        value: data.value,
+        status: data[1].status,
+        error: data[1].reason,
       }];
     });
 }

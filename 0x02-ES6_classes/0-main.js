02cdfea9e4c9174b7a83792df852c3911b14ff3a
MayDan12 <oluwamayowadan@gmail.com>
2024-04-25 17:38:13 +0000

    good

--- 0x02-ES6_classes/0-main.js
+++ 0x02-ES6_classes/0-main.js
@@ -8,23 +8,29 @@
 // console.log(initializeRooms());
 
 
-import HolbertonCourse from "./2-hbtn_course.js";
+// import HolbertonCourse from "./2-hbtn_course.js";
 
-const c1 = new HolbertonCourse("ES6", 1, ["Bob", "Jane"])
-console.log(c1.name);
-c1.name = "Python 101";
-console.log(c1);
+// const c1 = new HolbertonCourse("ES6", 1, ["Bob", "Jane"])
+// console.log(c1.name);
+// c1.name = "Python 101";
+// console.log(c1);
 
-try {
-    c1.name = 12;
-} 
-catch(err) {
-    console.log(err);
-}
+// try {
+//     c1.name = 12;
+// } 
+// catch(err) {
+//     console.log(err);
+// }
 
-try {
-    const c2 = new HolbertonCourse("ES6", "1", ["Bob", "Jane"]);
-}
-catch(err) {
-    console.log(err);
-}
+// try {
+//     const c2 = new HolbertonCourse("ES6", "1", ["Bob", "Jane"]);
+// }
+// catch(err) {
+//     console.log(err);
+// }
+
+
+import Currency from "./3-currency.js";
+
+const dollar = new Currency('$', 'Dollars');
+console.log(dollar.displayFullCurrency());
---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,31 @@
+export default class Currency {
+  constructor(code, name) {
+    this._code = code;
+    this._name = name; 
+  }
+
+  get name() {
+    return this._name
+  }
+
+  get code() {
+    return this._code
+  }
+
+  set name(newName) {
+    if (typeof newName === 'string') {
+      this._name = newName;
+    }
+  }
+
+  set code(newCode) {
+    if (typeof newCode === 'string') {
+      this._code = newCode;
+    }
+  }
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`
+  }
+
+}

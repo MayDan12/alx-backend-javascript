e708b449df6837da191c86bb340fef87ae050e72
MayDan12 <oluwamayowadan@gmail.com>
2024-04-26 01:28:43 +0000

    100 EV

--- 0x02-ES6_classes/0-main.js
+++ 0x02-ES6_classes/0-main.js
@@ -43,16 +43,43 @@
 // console.log(p.displayFullPrice());
 
 
-import Building from './5-building.js';
+// import Building from './5-building.js';
 
-const b = new Building(100);
-console.log(b);
+// const b = new Building(100);
+// console.log(b);
 
-class TestBuilding extends Building {}
+// class TestBuilding extends Building {}
 
-try {
-    new TestBuilding(200)
-}
-catch(err) {
-    console.log(err);
-}
+// try {
+//     new TestBuilding(200)
+// }
+// catch(err) {
+//     console.log(err);
+// }
+
+// import SkyHighBuilding from './6-sky_high.js';
+
+// const building = new SkyHighBuilding(140, 60);
+// console.log(building.sqft);
+// console.log(building.floors);
+// console.log(building.evacuationWarningMessage());
+
+// import Airport from "./7-airport.js";
+
+// const airportSF = new Airport('San Francisco Airport', 'SFO');
+// console.log(airportSF);
+// console.log(airportSF.toString());
+
+// import HolbertonClass from "./8-hbtn_class.js";
+
+// const hc = new HolbertonClass(12, "Mezzanine")
+// console.log(Number(hc));
+// console.log(String(hc));
+
+import EVCar from './100-evcar.js';
+
+const ec1 = new EVCar("Tesla", "Turbo", "Red", "250");
+console.log(ec1);
+
+const ec2 = ec1.cloneCar();
+console.log(ec2);
---
+++ 0x02-ES6_classes/100-evcar.js
@@ -0,0 +1,23 @@
+import Car from './10-car';
+
+export default class EVCar extends Car {
+  constructor(brand, motor, color, range) {
+    super(brand, motor, color);
+    this.range = range;
+  }
+
+  get range() {
+    return this._range;
+  }
+
+  set range(value) {
+    this._range = value;
+  }
+
+  cloneCar() {
+    const Species = new Car();
+    const dan = this.constructor[Symbol.species];
+    console.log(dan);
+    return Species;
+  }
+}

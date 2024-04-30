4de6946e2387ebc2fe2f010f14a3ae2adb0099c5
MayDan12 <oluwamayowadan@gmail.com>
2024-04-30 14:49:56 +0000

    clean 8 - 9

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,13 +1,11 @@
 export default function cleanSet(setobj, startstring) {
   const valuesStartingWithPrefix = [];
-  
-  setobj.forEach(value => {
+  setobj.forEach((value) => {
     if (value.startsWith(startstring)) {
       valuesStartingWithPrefix.push(value.slice(startstring.length));
     } else {
-      setobj.clear()
+      setobj.clear();
     }
   });
-  
   return valuesStartingWithPrefix.join('-');
 }

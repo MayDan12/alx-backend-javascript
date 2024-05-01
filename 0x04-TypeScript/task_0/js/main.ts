5bf3f915fe34768251ebc8cd35dee4160cc9c19f
MayDan12 <oluwamayowadan@gmail.com>
2024-05-01 09:48:39 +0000

    Started learning TypeScript

---
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -0,0 +1,13 @@
+interface Student {
+  firstName: string;
+  lastName: string;
+  age: number;
+  location: string;
+}
+
+const student1: Student = {
+  firstName: "Danny",
+  lastName: "Mayo",
+  age: 20,
+  location: 'lagos',
+}

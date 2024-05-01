9da84d50c5f23e702f96bd2117a21976f9b7f88f
MayDan12 <oluwamayowadan@gmail.com>
2024-05-01 11:10:16 +0000

    Created a Student interface

--- 0x04-TypeScript/task_0/js/main.ts
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -3,11 +3,36 @@ interface Student {
   lastName: string;
   age: number;
   location: string;
-}
+};
 
 const student1: Student = {
   firstName: "Danny",
   lastName: "Mayo",
   age: 20,
   location: 'lagos',
-}
+};
+
+const student2: Student = {
+  firstName: "Ranmi",
+  lastName: "Precious",
+  age: 19,
+  location: 'lagos',
+};
+
+const studentsList: Student[] = [student1, student2];
+
+document.addEventListener("DOMContentLoaded", () => {
+  const table = document.createElement("table");
+
+  // Iterate over the students array and append a new row for each student
+  studentsList.forEach(student => {
+    const row = table.insertRow();
+    const cell1 = row.insertCell();
+    const cell2 = row.insertCell();
+    
+    cell1.textContent = student.firstName;
+    cell2.textContent = student.location;
+  });
+
+  document.body.appendChild(table);
+});

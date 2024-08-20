a8e3056201adad95a5348a36e0253b337503a6c1
MayDan12 <oluwamayowadan@gmail.com>
2024-08-20 23:28:06 +0000

    teath

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,23 +1,23 @@
 // 1-stdin.js
-process.stdout.write("Welcome to Holberton School, what is your name?\n");
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 if (process.stdin.isTTY) {
   // For TTY input (interactive input)
-  process.stdin.on("data", (data) => {
+  process.stdin.on('data', (data) => {
     const name = data.toString().trim();
     process.stdout.write(`Your name is: ${name}\n`);
-    process.stdout.write("This important software is now closing\n");
+    process.stdout.write('This important software is now closing\n');
     process.exit();
   });
 } else {
   // For non-TTY input (piped input)
-  process.stdin.on("data", (data) => {
+  process.stdin.on('data', (data) => {
     const name = data.toString().trim();
     process.stdout.write(`Your name is: ${name}\n`);
     process.exit();
   });
 
-  process.on("exit", () => {
-    process.stdout.write("This important software is now closing\n");
+  process.on('exit', () => {
+    process.stdout.write('This important software is now closing\n');
   });
 }
